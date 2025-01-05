e404420355c65aaba5e240f1097118268c02eb57
Killian <martykillian7@gmail.com>
2025-01-05 10:32:42 +0100

    Building Front end

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+  return (
+    <div className="bg-white border-t">
+        <div className="mx-auto py-10">
+            <p className="text-center text-sm text-black">
+                &copy; 2023 Modea Store, Inc. All rights reserved
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
