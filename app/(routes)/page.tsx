91c9bf84480eba11b2b6baf79342cdfc890313f1
Killian <martykillian7@gmail.com>
2025-01-05 11:37:55 +0100

    Billboard Fetch

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,18 @@
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+import getBillboard from "@/actions/get-billboard";
 
-const HomePage = () => {
+export const revalidator = 0;
+
+const HomePage = async () => {
+    const billboard = await getBillboard("e5d43e61-e8f7-47c7-a27e-955bb1aa16e7");
+
   return (
-    <div>Hello Store</div>
+    <Container>
+        <div className="space-y-10 pb-10">
+            <Billboard data={billboard}/>
+        </div>
+    </Container>
   )
 }
 
