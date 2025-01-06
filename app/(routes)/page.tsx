86dcf562573d77ff2dc6e0b06316205dd8f98a9e
Killian <martykillian7@gmail.com>
2025-01-06 10:30:35 +0100

    Fetching Product cards

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,16 +1,22 @@
 import Container from "@/components/ui/container";
 import Billboard from "@/components/billboard";
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
 
 export const revalidator = 0;
 
 const HomePage = async () => {
+    const products = await getProducts({ isFeatured: true });
     const billboard = await getBillboard("e5d43e61-e8f7-47c7-a27e-955bb1aa16e7");
 
   return (
     <Container>
         <div className="space-y-10 pb-10">
             <Billboard data={billboard}/>
+            <div className="flex flex-xol gap-x-8 px-4 sm:px-6 lg:px-8">
+                <ProductList title="Featured Products" items={products}/>
+            </div>
         </div>
     </Container>
   )
