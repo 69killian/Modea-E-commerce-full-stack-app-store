dae410110032278a2f988a7b89846100fd1d0838
Killian <martykillian7@gmail.com>
2025-01-08 14:58:22 +0100

    delete revalidator

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -4,8 +4,6 @@ import getBillboard from "@/actions/get-billboard";
 import getProducts from "@/actions/get-products";
 import ProductList from "@/components/product-list";
 
-export const revalidator = 0;
-
 const HomePage = async () => {
     const products = await getProducts({ isFeatured: true });
     const billboard = await getBillboard("e5d43e61-e8f7-47c7-a27e-955bb1aa16e7");
@@ -14,9 +12,11 @@ const HomePage = async () => {
     <Container>
         <div className="space-y-10 pb-10">
             <Billboard data={billboard}/>
+
             <div className="flex flex-xol gap-x-8 px-4 sm:px-6 lg:px-8">
                 <ProductList title="Featured Products" items={products}/>
             </div>
+
         </div>
     </Container>
   )
